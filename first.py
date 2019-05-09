00d8b0075af9f9042b2ed2fdd7dd31d7cbd772ba
Tordai Janos <janos.tordai@ZAMG.local>
2019-05-09 12:52:47 +0200

    celsius to kelvin function + test

--- first.py
+++ first.py
@@ -1,1 +1,9 @@
 # main code reset 12:48
+
+c = [12.3, 14.5, 17.2, 1.3]
+
+def celsius_to_kelvin(celsius = None):
+	result = [c+273.15 for c in celsius]
+	return result
+	
+print(celsius_to_kelvin(celsius = c))
