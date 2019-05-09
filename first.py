676ba9a1c985b16104f91942d914d3cccabc474a
Tordai Janos <janos.tordai@ZAMG.local>
2019-05-09 14:49:54 +0200

    func3 added

--- first.py
+++ first.py
@@ -1,4 +1,10 @@
-# developer code reset 14:35
+c = [12, 13, 14]
+
+def func1(inp = None):
+	return [i+1 for i in inp]
 
 def func2(inp = None):
 	return [i-1 for i in inp]
+
+def func3(inp = None):
+	return [i*2 for i in inp]
