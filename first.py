111898827dee5d1311975678b4061dcc7b6047bb
Tordai Janos <janos.tordai@ZAMG.local>
2019-05-09 12:49:26 +0200

    developer reset

--- first.py
+++ first.py
@@ -1,11 +1,1 @@
-# main code
-
-for i in range(10):
-	print(i)
-	
-print('Finish')
-# new code for testing develop branch
-print("develop")
-
-# second test
-print("second")
+# developer code reset 12:49
