2c812300a53546004c7329351babd8eb0f6dee8f
Tordai Janos <janos.tordai@ZAMG.local>
2019-05-09 14:35:49 +0200

    developer reser again

--- first.py
+++ first.py
@@ -1,15 +1,1 @@
-# developer code reset 12:49
-
-c = [12.3, 14.5, 17.2, 1.3]
-k = [290, 310, 273.15, 280]
-
-def celsius_to_kelvin(celsius = None):
-	result = [c+273.15 for c in celsius]
-	return result
-	
-def kelvin_to_celsius(kelvin = None):
-	result = [k-273.15 for k in kelvin]
-	return result
-	
-print(celsius_to_kelvin(celsius = c))
-print(kelvin_to_celsius(kelvin = k))
+# developer code reset 14:35
