d7c72c76e70c1e4c3d270d1ade5c0ea937eb9f05
Tordai Janos <janos.tordai@ZAMG.local>
2019-05-09 12:48:37 +0200

    reset master

--- first.py
+++ first.py
@@ -1,17 +1,1 @@
-# main code
-
-for i in range(10):
-	print(i)
-	
-print('Finish')
-<<<<<<< HEAD
-
-# added master test
-print("test master")
-
-# new code for testing develop branch
-print("develop")
-
-# second test
-print("second")
->>>>>>> develop
+# main code reset 12:48
