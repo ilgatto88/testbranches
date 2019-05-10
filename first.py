267f68c885906443a1373e965348cf6c3d32a135
Tordai Janos <janos.tordai@ZAMG.local>
2019-05-10 14:19:48 +0200

    fix2

--- first.py
+++ first.py
@@ -4,7 +4,7 @@ def func1(inp = None):
 	return [i+1 for i in inp]
 
 def func2(inp = None):
-	return [i-1 for i in inp]
+	return [i-8 for i in inp] # bugfix 2 for release 1.2 @JT
 
 def func3(inp = None):
 	return [i*2 for i in inp]
